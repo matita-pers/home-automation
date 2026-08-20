c18ce65a19d124072eb503e7d8e49124e11abb17
Matita008 <155455608+Matita008@users.noreply.github.com>
2026-08-21 00:26:20 +0200

    web: feat: login/logout
    - created login, logout and session (current session info) api endpoints
    - added serving of local files when 'VERCEL' is not set
    - configured session lifetime of 1 week (also reads secret_key from env
    - some db edits (we should call the db the least possible

--- webserver/app.py
+++ webserver/app.py
@@ -1,7 +1,29 @@
 from flask import Flask
+import os
 
-app = Flask(__name__)
+import login
+
+app = Flask(__name__, static_folder="../static")
+
+app.secret_key = os.environ.get("SECRET_KEY")
+app.config['session.permanent'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = 60 * 60 * 24 * 7 # a week
+
+login.load(app)
 
 @app.route("/api/health")
 def health():
     return "OK"
+
+# vercel serves static files directly, but for testing this is needed to serve them
+if not os.environ.get("VERCEL") == 1:
+    @app.route("/")
+    def index():
+        return app.send_static_file("html/index.html")
+
+    @app.route("/<path:path>")
+    def static_proxy(path):
+        print(path)
+        if path.endswith(".html"):
+            return app.send_static_file("html/" + path)
+        return app.send_static_file(path)
